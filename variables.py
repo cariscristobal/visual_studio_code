96d42f753b0b51193b6e1634b06c3fa7ab063b76
Cristobal Caris <cristobal.caris@gmail.com>
2018-09-05 12:20:04 -0300

    cambios en tipo de comentarios

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 curso_prueba = "Python 3"
-curso_prueba = 99 """ prueba de cambios """
-curso_prueba = True ""prueba de variable a Boolean""
+curso_prueba = 99 #prueba de cambios
+curso_prueba = True #prueba de variable a Boolean
 print(curso_prueba)
 print("Test")
