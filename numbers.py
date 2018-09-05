aca0619f04fd0617615887a9e1a38e5c82320229
Cristobal Caris <cristobal.caris@gmail.com>
2018-09-05 12:17:23 -0300

    Prueba de operaciones aritmeticas en python vsc

---
+++ numbers.py
@@ -0,0 +1,17 @@
+number_one = 10
+number_two = 4
+
+result = number_one + number_two
+print ("Suma", result)
+
+result = number_one - number_two
+print ("Resta", result)
+
+result = number_one * number_two # Regresa un número flotante  
+print ("Multiplicación", result)
+
+result = number_one // number_two # Regresa un número entero
+print("División parte entera", result)
+
+result = number_one ** number_two # Regresa el exponencial de un número
+print("Exponencial", result)
